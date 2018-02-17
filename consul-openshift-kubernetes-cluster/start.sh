8f5f9befa808503f6a0f4f006a7cff096c90acc2
rsthakur83 <rsthakur83@yahoo.com>
2018-02-17 01:00:03 +0100

    consul cluster on  openshift kubernetes-
    consul cluster on  openshift kubernetes-

---
+++ consul-openshift-kubernetes-cluster/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+MY_POD_NAME=`hostname`
+POD_IP=`ifconfig |grep 172|awk '{print $2}'`
+val=$POD_IP
+
+
+if [ "$MY_POD_NAME" = "consul-0" ];then
+    echo "Consul-0"
+    sed -i "s/IP/${val}/g" /etc/consul/consul0.json
+    consul agent -server -config-dir /etc/consul/consul0.json
+
+elif [ "$MY_POD_NAME" = "consul-1" ];then
+    echo "Consul-1"
+    sed -i "s/IP/${val}/g" /etc/consul/consul1.json
+    consul agent -server -config-dir /etc/consul/consul1.json
+
+elif [ "$MY_POD_NAME" = "consul-2" ];then
+    echo "Consul-2"
+    sed -i "s/IP/${val}/g" /etc/consul/consul2.json
+    consul agent -server -config-dir /etc/consul/consul2.json
+else
+   echo "Consul-3"
+    sed -i "s/IP/${val}/g" /etc/consul/consul.json
+    consul agent -server -config-dir /etc/consul/consul.json
+fi
