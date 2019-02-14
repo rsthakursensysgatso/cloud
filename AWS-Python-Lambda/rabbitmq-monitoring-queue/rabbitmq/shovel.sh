3b10b453c7a81f374aa2b084471c5dd9f2ac2a63
rsthakur83 <rsthakur83@yahoo.com>
2019-02-14 09:47:05 +0000

    Update shovel.sh

--- AWS-Python-Lambda/rabbitmq-monitoring-queue/rabbitmq/shovel.sh
+++ AWS-Python-Lambda/rabbitmq-monitoring-queue/rabbitmq/shovel.sh
@@ -4,7 +4,7 @@
 
 a=$(python3.6 rabbitmq/shovel.py)
 
-curl -i -u lewsupnp:$a -H "content-type:application/json" -XPUT -d'{"value":{"src-uri":"amqp://lewsupnp:reFMl1c1FfD-7D68ISDD6Uxq5GecprYS@duckbill.rmq.cloudamqp.com/lewsupnp","src-queue":"devops-testing-error","dest-uri":"amqp://lewsupnp:reFMl1c1FfD-7D68ISDD6Uxq5GecprYS@duckbill.rmq.cloudamqp.com/lewsupnp","dest-queue":"devops-testing-error-archive"}}' https://duckbill.rmq.cloudamqp.com/api/parameters/shovel/lewsupnp/my-shovel
+curl -i -u lewsupnp:$a -H "content-type:application/json" -XPUT -d'{"value":{"src-uri":"amqp://lewsupnp:-7D68ISDD6Uxq5GecprYS@duckbill.rmq.cloudamqp.com/lewsupnp","src-queue":"devops-testing-error","dest-uri":"amqp://lewsupnp:reFMl1c1FfD-7D68ISDD6Uxq5GecprYS@duckbill.rmq.cloudamqp.com/lewsupnp","dest-queue":"devops-testing-error-archive"}}' https://duckbill.rmq.cloudamqp.com/api/parameters/shovel/lewsupnp/my-shovel
 
 sleep 5
 
