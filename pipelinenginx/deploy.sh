4a0d9a4fab3517433864a53ac9c95ac608c1097d
rsthakur83 <rsthakur83@yahoo.com>
2020-04-19 10:13:09 +0200

    Update deploy.sh

--- pipelinenginx/deploy.sh
+++ pipelinenginx/deploy.sh
@@ -1,4 +1,13 @@
 #/bin/bash
 
 kubectl apply -f myapp/nginx.yaml
-kubectl patch deployment mynginx -p '{"spec":{"template":{"metadata":{"labels":{"build": "ravi"}}}}}'
+kubectl get deployment -n myapp mynginx
+if [ $? -eq 0 ]
+then
+
+echo "Patching Deployment mynginx"
+kubectl patch deployment -n myapp mynginx -p '{"spec":{"template":{"metadata":{"labels":{"build": "ravi"}}}}}'
+else
+
+echo "Deployment mynginx not exist and creating"
+fi
