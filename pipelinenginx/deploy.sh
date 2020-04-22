4360ef8e5eb83d451e98a478e51de0b5da31a166
rsthakur83 <rsthakur83@yahoo.com>
2020-04-22 21:51:18 +0200

    Update deploy.sh

--- pipelinenginx/deploy.sh
+++ pipelinenginx/deploy.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-kubectl apply -f /home/ubuntu/myapp/nginx.yaml
-kubectl apply -f /home/ubuntu/myapp/config-map.yaml
-kubectl apply -f /home/ubuntu/myapp/hpa.yaml
+kubectl apply -f /home/kube/myapp/nginx.yaml
+kubectl apply -f /home/kube/myapp/config-map.yaml
+kubectl apply -f /home/kube/myapp/hpa.yaml
 kubectl get deployment -n myapp mynginx
 if [ $? -eq 0 ]
 then
