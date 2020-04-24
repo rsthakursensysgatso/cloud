655a61af18a9741c33a3781ce17574095c90d308
U-BUDAPEST\Ravinder_Thakur <Ravinder_Thakur@EPNLAMSW0207.budapest.epam.com>
2020-04-24 13:48:00 +0200

    flaskap pipeline

---
+++ pipelinflaskapp/deploy.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+kubectl apply -f /home/kube/myapp/flaskapp.yaml
+kubectl apply -f /home/kube/myapp/config-map.yaml
+kubectl apply -f /home/kube/myapp/hpa.yaml
+kubectl get deployment -n myapp myflaskapp
+if [ $? -eq 0 ]
+then
+
+echo "Patching Deployment myflaskapp"
+kubectl patch deployment -n myapp myflaskapp -p '{"spec":{"template":{"metadata":{"labels":{"build": "'"`date +"%H%M%S"`"'"}}}}}'
+else
+
+echo "Deployment myflaskapp not exist so created above"
+fi
